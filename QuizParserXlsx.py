52c551a463707900541852a23dce627763c3519b
Cole <cturner5@uccs.edu>
2019-05-10 10:19:33 -0600

    Most of excel parser

--- QuizParserXlsx.py
+++ QuizParserXlsx.py
@@ -1,1 +1,31 @@
-#object is to return a dictionary where in each key is a question and its value is its answer
+import openpyxl
+
+
+class Quiz:
+    def __init__(self):
+        self.fileName = "" #name of file to be used
+        self.questions = [] #list of questions
+        self.answers = [] #list of answers, answers list corresponds with the questions in the questions list
+        self.quiz = {} #dictionary made by combining questions and answers
+
+    def parseQuestionsXlsx(self, fileName):
+        questions = []
+        location = "quizFiles\\" + fileName
+        quizFile = openpyxl.load_workbook(location)
+        sheet_names = quizFile.sheetnames
+        sheet = quizFile[sheet_names[0]]
+
+        tempString = ""
+        for cellObj in sheet['B']:
+            if cellObj.value != None and sheet['C'][cellObj.row - 1].value == None:
+                tempString = tempString + str(cellObj.value) + "\n"
+            elif cellObj.value != None and sheet['C'][cellObj.row - 1].value != None:
+                tempString = tempString + str(cellObj.value) + " " + str(sheet['C'][cellObj.row - 1].value) + "\n"
+            elif cellObj.value == None:
+                questions.append(tempString)
+
+        for x in range(len(questions)):
+            print(questions[x])
+
+
+Quiz.parseQuestionsXlsx("",'textxlsx.xlsx')
